9ac07701f250e9bce7ac925a2e4cf4458e6021f9
sleepyhead <aurangzebalam896@gmail.com>
2023-04-17 00:24:26 +0530

    database link

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const https=require("https");
-const mongoose=require('mongoose');
-// var MongoClient = require('mongodb').MongoClient;
+const db=require("./dbatlas");
 const app=express();
-// var url = "mongodb://localhost:27017/postsDB";
 
-// MongoClient.connect(url, function(err, db) {
-//     if (err) throw err;
-//     console.log("Database created!");
-//     db.close();
-//   });
-
-// const url="mongodb://localhost:3000/postsDB";
-// mongoose.connect(url,{
-//     useNewUrlParser:true,useUnifiedTopology:true
-// },(err)=>{
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log("successfullly connected.");
-//     }
-// })
 app.set("view engine","ejs")
 const posts=[];
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true}));
 
+
 app.get("/",function(req,res){
     res.sendFile(__dirname+ "/index.html");
 })
-
+// db.connectDB();
 app.post("/",function(req,res){
     var content=req.body.content;
     var title=req.body.title;
